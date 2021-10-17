dd0b75b574cded71e37cf50c6e8f0d71947d4467
fajlay203 <fajlay203.fr@gmail.com>
2021-10-17 22:16:02 +0600

    mid task

--- MID_LAB_EXAM/home.php
+++ MID_LAB_EXAM/home.php
@@ -9,12 +9,18 @@
 	<title>Admin Page</title>
 </head>
 <body>
+       <h1 style="background-color:powderblue;">Welcome to Admin Page</h1>
+  <center>
+
+    <table>
 	<h1>Welcome to Bob!</h1>
 	<table style="width:100%">
 	<a href="profile.html">Profile </a> | 
+	<a href="viewuser.html">View User </a> | 
 	<a href="changepass.html">Change Password </a> |
 	<a href="logout.php">logout </a>
 	</table>
+	</center>
 </body>
 </html>
 

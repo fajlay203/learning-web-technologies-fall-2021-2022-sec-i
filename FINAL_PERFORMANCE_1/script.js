fe1c2c474bf4d405c90baa37fe2571e4a5ac846a
fajlay203 <fajlay203.fr@gmail.com>
2021-11-21 16:01:28 +0600

    cal

--- FINAL_PERFORMANCE_1/script.js
+++ FINAL_PERFORMANCE_1/script.js
@@ -23,3 +23,13 @@ function getFormattedNumber(num){
 	var value = n.toLocaleString("en");
 	return value;
 }
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="clear"){
+			printHistory("");
+			printOutput("");
+		}
